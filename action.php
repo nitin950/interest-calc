b36d26e6b880b5d1ec6a3a13f6273eb19bb66cd5
nitin950 <58341752+nitin950@users.noreply.github.com>
2020-03-28 22:18:09 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,28 @@
+<?php
+           $host = "localhost";
+           $user = "root";
+           $password = "";
+           $dbname = "test";;
+           $con = mysqli_connect($host, $user, $password, $dbname);
+           if (!$con) {
+             die("Connection failed: " . mysqli_connect_error());
+           }
+           else
+           {
+                  $sno = 0;
+                  $amount = $_POST["amount"];
+                  $rate = $_POST["rate"];
+                  $time = $_POST["time"];
+		  $int = ($amount*$rate*$time)/100;
+                  $sql = "INSERT INTO interestcal(amount, rate, time, interest)
+                  VALUES ($amount, $rate, $time, $int)";
+
+                  if ($con->query($sql) === TRUE) {
+                           
+
+                             header("Location: http://127.0.0.1/projects/index.php");
+                      } else {
+                           echo "Error: " . $sql . "<br>" . $con->error;
+                      }
+         }
+?>  
